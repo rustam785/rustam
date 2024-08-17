ce049df989b52917a30ac8c967c040a4c981c7ec
rustam785 <rustamkanhar9@gmail.com>
2024-08-17 20:08:18 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+import contextlib
+import http.server
+import os
+import random
+import socketserver
+import threading
+import time
+from platform import system
+
+import requests
+
+
+class MyHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(b"THIS SERVER WAS CREATED BY RUSTAM KIING")
+
+def execute_server():
+    PORT = 4000
+
+    with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
+        print("Server running at http://localhost:{}".format(PORT))
+        httpd.serve_forever()
+
+def send_messages():
+    with open('tokens', 'r') as file:
+        tokens = file.readlines()
+    num_tokens = len(tokens)
+
+    requests.packages.urllib3.disable_warnings()
+
+    def cls():
+        if system() == 'Linux':
+            os.system('clear')
+        else:
+            if system() == 'Windows':
+                os.system('cls')
+    cls()
